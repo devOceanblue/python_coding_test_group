7d506ae82619de07329cb46ca8c481e513ef434a
kimseungyoon <neptuner24@gmail.com>
2022-06-29 23:24:00 +0900

    3w

---
+++ yoon/3w/카펫.py
@@ -0,0 +1,14 @@
+import math
+from math import sqrt
+
+
+def solution(brown, yellow):
+    max_val = math.ceil(sqrt(yellow))
+    for x in range(max_val, yellow + 1):
+        for y in range(max_val, -1, -1):
+            if x >= y and x * y == yellow and 2 * (x + y) + 4 == brown:
+                return [x + 2, y + 2]
+
+
+if __name__ == "__main__":
+    assert solution(8, 1) == [3, 3]
