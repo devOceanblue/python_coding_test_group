90f26908c822790e3bd66b52175bb9a46bbdffce
kimseungyoon <neptuner24@gmail.com>
2022-09-13 22:50:13 +0900

    누적합

--- yoon/누적합/파괴되지 않은 건물.py
+++ yoon/누적합/파괴되지 않은 건물.py
@@ -1,17 +1,30 @@
 def solution(board, skill):
     answer = 0
+    tmp = [[0] * (len(board[0]) + 1) for _ in range(len(board) + 1)]  # 누적합 기록을 위한 배열
 
     for type, r1, c1, r2, c2, degree in skill:
-        # (r1, c1) ~ (r2, c2) 값 추가
-        for y in range(r1, r2 + 1, 1):
-            for x in range(c1, c2 + 1, 1):
-                board[y][x] += (
-                    degree if type == 2 else -degree
-                )  # type == 2 인 경우 -degree 추가
+        # 누적합 기록, 부호에 주의할 것
+        tmp[r1][c1] += degree if type == 2 else -degree
+        tmp[r1][c2 + 1] += -degree if type == 2 else degree
+        tmp[r2 + 1][c1] += -degree if type == 2 else degree
+        tmp[r2 + 1][c2 + 1] += degree if type == 2 else -degree
 
-    for row in board:
-        answer += len(
-            list(filter(lambda x: x > 0, row))
-        )  # x > 0 이상인 리스트의 길이를 answer에 추가함
+    # 행 기준 누적합
+    for i in range(len(tmp) - 1):
+        for j in range(len(tmp[0]) - 1):
+            tmp[i][j + 1] += tmp[i][j]
+
+    # 열 기준 누적합
+    for j in range(len(tmp[0]) - 1):
+        for i in range(len(tmp) - 1):
+            tmp[i + 1][j] += tmp[i][j]
+
+    # 기존 배열과 합함
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            board[i][j] += tmp[i][j]
+            # board에 값이 1이상인 경우 answer++
+            if board[i][j] > 0:
+                answer += 1
 
     return answer
